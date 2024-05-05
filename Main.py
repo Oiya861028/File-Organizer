ce9ffa8120de855d0e39d713f81485e1384f73af
yubinx <yubinxie222@gmail.com>
2024-05-05 12:57:11 -0400

    Fixed bug where the file is not grouped correctly by type

--- Main.py
+++ Main.py
@@ -3,12 +3,12 @@ from os.path import isfile, join
 import os
 import shutil
 
-file_path='C:/Users/yubin/OneDrive/DeskTop/Test'
+file_path='C:/Users/yubin/Downloads'
 files = [f for f in listdir(file_path) if isfile(join(file_path, f))]
 file_type = []
 file_dict = {}
 for f in files:
-    filetype = f.split(".")[1]
+    filetype = f.split(".")[-1]
     if(filetype not in file_type):
         file_type.append(filetype)
         new_folder_location = file_path+'/'+filetype
@@ -17,6 +17,14 @@ for f in files:
             continue
         else:
             os.makedirs(new_folder_location)
-
+i = 1
+for file in files:
+    fileType = file.split(".")[-1]
+    source = file_path+'/'+file
+    if(fileType in file_dict.keys()):
+        target = file_dict[str(fileType)]
+        shutil.move(source, target)
+        print(i," - ",source+">>>"+target)
+    i+=1
 
 
--- Revert.py
+++ Revert.py
@@ -7,6 +7,7 @@ file_path='C:/Users/yubin/Downloads'
 folders = [f for f in listdir(file_path)]
 i = 1
 for folder in folders:
+    #Goes into each folder and iterate all out of the current folder 
     current_folder = file_path+'/'+folder
     files = [f for f in listdir(current_folder)]
     for file in files:
